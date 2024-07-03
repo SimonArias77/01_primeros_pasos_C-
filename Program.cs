8dde19ac0155fef1027f6cefcf699d5c59a95e88
Daniel Montiel <danielmontiel1600@gmail.com>
2024-07-03 07:39:37 -0500

    feat:se avanza en ejercicios

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿
+
+// PRIMER TALLER C#
+
+// // 1. Escribe un programa que imprima “¡Hola, mundo!" en la consola
+// Console.WriteLine("¡Hola, mundo!");
+
+
+// // 2. Declara un int y un string, asignarles valores e imprímelos.
+// int numero = 45;
+// string nombre = "David"; 
+
+// Console.WriteLine($"{numero}, {nombre}");
+
+
+// // 3. Convierte un string a mayúsculas y a minúsculas.
+// string text = "Primer Taller";
+
+// //Convertir a mayusculas
+// Console.WriteLine(text.ToUpper());
+
+// //Convertir a minusculas
+// Console.WriteLine(text.ToLower());
+
+
+// // 4. Declara dos int, realizar suma, resta, multiplicación y división, y muestra los resultados.
+// int num1 = 10;
+// int num2 = 5;
+
+// Console.WriteLine($"Suma: {num1+num2}");
+// Console.WriteLine($"Resta: {num1-num2}");
+// Console.WriteLine($"Multiplicación: {num1*num2}");
+// Console.WriteLine($"División: {num1/num2}");
+
+
+// // 5. Declara dos int, realiza comparaciones de igualdad, desigualdad, mayor que y menor que, y muestra los resultados.
+// int num3 = 10;
+// int num4 = 8;
+
+// Console.WriteLine($"igualdad: {num3 == num4}");
+// Console.WriteLine($"Desigualdad: {num3 != num4}");
+// Console.WriteLine($"Mayor que: {num3 > num4}");
+// Console.WriteLine($"Menor que: {num3 < num4}");
+
+
+// // 6. Busca una palabra específica en un string y reemplazarla por otra.
+// string text1 = "Hoy gana Brasil";
+// Console.WriteLine(text1);
+// text1 = text1.Replace("Brasil","Colombia");
+// Console.WriteLine(text1);
+
+
+// // 7. Pide al usuario su nombre y edad, luego imprime un mensaje con esta información.
+// Console.WriteLine("Digite su nombre: ");
+// string nombre = Console.ReadLine();
+
+// Console.WriteLine("Digite su edad: ");
+// int edad = int.Parse(Console.ReadLine());
+
+// Console.WriteLine($"Hola {nombre}, tienes {edad} años.");
+
+
+// // 8. Declara un int y escribe un programa que indique si es mayor, menor o igual a 10.
+// int num = 20;
+
+// Console.WriteLine($"igualdad: {num == 10}");
+// Console.WriteLine($"Mayor que: {num > 10}");
+// Console.WriteLine($"Menor que: {num < 10}");
+
+
+// // 9. Declara dos variables double, realiza suma, resta, multiplicación y división, y muestra los resultados.
+// double num1 = 10.5;
+// double num2 = 5.0;
+
+// Console.WriteLine($"Suma: {num1+num2}");
+// Console.WriteLine($"Resta: {num1-num2}");
+// Console.WriteLine($"Multiplicación: {num1*num2}");
+// Console.WriteLine($"División: {num1/num2}");
+
+
+// // 10. Declarar dos variables string, concatenarlas y mostrar el resultado.
+// string text1 = "Hola";
+// string text2 = "mundo";
+
+// Console.WriteLine($"{text1} {text2}");
+
+
+// // 11. Declara una variable int, conviértela a double y muestra ambos valores.
+// int numeroEntero = 10;
+
+// double numeroDecimal = Convert.ToDouble(numeroEntero);
+
+
+// 12. Escribe un programa que pida al usuario un número y muestre si es positivo, negativo o cero.
+Console.WriteLine($"Por favor escriba un número: ");
+int numero1 = ;
+
+// 13. Usa un bucle for para imprimir los números del 1 al 10.
+// 14. Elimina los espacios en blanco al inicio y al final de un string, y formatea el string para que
+// empiece con mayúscula y el resto en minúsculas.
+// 15. Escribe un programa que pida al usuario un número y muestre su tabla de multiplicar del 1
+// al 10.
+// 16. Concatena dos strings y muestra la longitud de cada uno.
+// 17. Extrae una subcadena de un string y muestra la subcadena y su longitud.  
